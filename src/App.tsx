2ad154f63f37c8bf636487edfc4a2092a393cee5
leosh1d <leosh1dddd@gmail.com>
2024-05-05 23:43:18 +0300

    resticted

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import sini4ka from './assets/sini4ka.jpg'
 import './App.css'
 import {Footer} from "./components/footer.tsx";
+import {Input, Stack} from "@chakra-ui/react";
 
 function App() {
 
   return (
     <>
-      <div>
+      <Stack bg={'#790179'}>
           <img src={sini4ka} className="logo" alt="React logo" />
+
+          <Stack px={8} mt={4}>
+              <Input placeholder='cvv code' border={`5px solid`}  p={8} fontSize={'3rem'} _placeholder={{color: '#B914FF'}} color={'#14FFF1'} />
+          </Stack>
+
           <Footer/>
-      </div>
+      </Stack>
     </>
   )
 }
