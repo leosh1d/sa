0ae7e2e30e5c6e96fb70f15d48d3e1447b35e04e
leosh1d <leosh1dddd@gmail.com>
2024-08-21 01:44:13 +0300

    try to fix prod vk auth

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,3 +1,3 @@
 export const baseDomainDev = "http://localhost/account";
 export const baseDomainProd = "https://studaktivbi.ru/account"
-export const baseDomain = process.env.NODE_ENV === "production" ? baseDomainProd : baseDomainDev;
+export const baseDomain = process.env.NODE_ENV === "development" ? baseDomainDev : baseDomainProd;
