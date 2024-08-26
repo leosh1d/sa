5751a0513e2d15c2e440bdda0c48157e7605de31
leosh1d <leosh1dddd@gmail.com>
2024-08-26 14:41:06 +0300

    fix building

--- src/state/common/appSlice/appSlice.ts
+++ src/state/common/appSlice/appSlice.ts
@@ -1,6 +1,9 @@
-import {AppSliceActions, AppSliceData} from "@/state/common/appSlice/appTypes";
+import {AppSliceData} from "@/state/common/appSlice/appTypes";
 
 export const appInitialData:AppSliceData = {
+    phone: undefined,
+    first_name: undefined,
+    last_name: undefined,
     vkIdConfig: undefined,
     isAuthorized: false,
     img: undefined,
