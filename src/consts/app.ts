682a272662d240780bcf17fd63a75594119db03b
leosh1d <leosh1dddd@gmail.com>
2024-11-16 01:05:03 +0300

    update price

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,4 +1,4 @@
 export const baseDomainDev = "http://localhost/success";
 export const baseDomainProd = "https://studaktivbi.ru/success"
-export const COST_BASIC = 1999
+export const COST_BASIC = 2299
 export const COST_VIP = 8999
